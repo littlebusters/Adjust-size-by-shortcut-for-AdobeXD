0fb8025dae8c32766428134315b70bec19426b8f
littlebusters <little.busters.reply@gmail.com>
2019-01-24 18:17:19 +0900

    WIP

--- main.js
+++ main.js
@@ -1,36 +1,117 @@
-const {createAlert} = require('./modules/helper');
+const {createSettingDialog, createAlert, readConfig, writeConfig, validateNum} = require('./modules/helper');
+const nudge = (async function() {
+    console.log('init');
+	// let nudgeValues = {'normal': 1, 'greatly': 10};
+	let nudgeValues = await readConfig();
+
+	let small = nudgeValues.normal;
+	let big   = nudgeValues.greatly;
+	console.log(small);
+	console.log(big);
+
+	return {
+		getValue: async function(size) {
+			switch (size) {
+        case 'small':
+            return small;
+        case 'big':
+            return big;
+        default:
+            return small;
+      }
+		},
+		update: async function(nudgeValues) {
+			small = nudgeValues.normal;
+			big   = nudgeValues.greatly;
+		}
+	};
+})();
 
 function shrinkWidth(selection) {
-    objectResize(selection.items, 'width', -1);
+    objectResize(selection.items, 'width', -nudge.getValue('small'));
 }
 function extendWidth(selection) {
-    objectResize(selection.items, 'width', 1);
+    objectResize(selection.items, 'width', nudge.getValue('small'));
 }
 function shrinkHeight(selection) {
-    objectResize(selection.items, 'height', -1);
+    objectResize(selection.items, 'height', -nudge.getValue('small'));
 }
 function extendHeight(selection) {
-    objectResize(selection.items, 'height', 1);
+    objectResize(selection.items, 'height', nudge.getValue('small'));
 }
 
 function shrinkGWidth(selection) {
-    objectResize(selection.items, 'width', -10);
+    objectResize(selection.items, 'width', -nudge.getValue('big'));
 }
 function extendGWidth(selection) {
-    objectResize(selection.items, 'width', 10);
+    objectResize(selection.items, 'width', nudge.getValue('big'));
 }
 function shrinkGHeight(selection) {
-    objectResize(selection.items, 'height', -10);
+    objectResize(selection.items, 'height', -nudge.getValue('big'));
 }
 function extendGHeight(selection) {
-    objectResize(selection.items, 'height', 10);
+    objectResize(selection.items, 'height', nudge.getValue('big'));
 }
+
+async function openSettingDialog() {
+    console.log('openSettingDialog');
+    const storedValue = await readConfig();
+    const dialog = createSettingDialog(storedValue);
+
+    console.log(nudge.getValue('small'));
+    console.log(nudge.getValue('big'));
+    nudge.update({'normal': 20, 'greatly': 8});
+	console.log(nudge.getValue('small'));
+	console.log(nudge.getValue('big'));
+
+
+
+    try {
+        const result = await dialog.showModal();
+        if ('reasonCanceled' !== result) {
+            let config = {};
+            config.normal = (validateNum(result.smallNudge)) ? Math.abs(result.smallNudge - 0) : defaultVal.smallNudge;
+            config.greatly = (validateNum(result.bigNudge)) ? Math.abs(result.bigNudge - 0) : defaultVal.bigNudge;
+            nudge.update(result.smallNudge, result.bigNudge)
+            await writeConfig(config);
+        } else {
+            console.log('Adjust size setting canceled.')
+        }
+		} catch(e) {
+        console.log(e);
+    }
+}
+
+
+
+// async function nudge() {
+// 	console.log('Set default value');
+// 	let defaultValue = await readConfig();
+// 	console.log(defaultValue);
+// 	let smallNudge = defaultValue.normal;
+// 	let bitNudge = defaultValue.greatly;
+//
+// 	const nudgeValue = (shift) => {
+// 			switch (shift) {
+// 				case 'small':
+// 					return smallNudge;
+// 				case 'big':
+// 					return bitNudge;
+// 			}
+// 		};
+// 	const update = (small, big) => {
+//       console.log('Update nudge value');
+//       smallNudge = small;
+//       bitNudge = big;
+//     };
+// };
+// let s = await nudge();
 
 // main
 function objectResize(sel, side, shift) {
     if (0 === sel.length) {
         const dialog = createAlert('title', 'message');
-		dialog.showModal();
+		    dialog.showModal();
         return false;
     }
 
@@ -57,6 +138,7 @@ module.exports = {
         "ShrinkGW": shrinkGWidth,
         "ExtendGW": extendGWidth,
         "ShrinkGH": shrinkGHeight,
-        "ExtendGH": extendGHeight
+        "ExtendGH": extendGHeight,
+        "Settings": openSettingDialog
     }
 };
--- modules/helper.js
+++ modules/helper.js
@@ -1,6 +1,92 @@
+const fs = require("uxp").storage.localFileSystem;
+const configFile = 'config.json';
 const dom = sel => document.querySelector(sel);
 const {uiLabel} = require('./i18l');
 const pluginIcon = '../images/adjust-size-by-shortcut-icon.png';
+
+function createSettingDialog(defaultVal) {
+	document.body.innerHTML = `
+<style>
+    dialog {
+        display: flex;
+        flex-direction: row-reverse;
+    }
+    #title {
+        margin-bottom: 16px;
+        padding-bottom: 7px;
+        border-bottom: 1px solid #ccc;
+        font-size: 16px;
+    }
+    label {
+        padding-top: 8px;
+    }
+    input {
+        width: 70px;
+    }
+    .formgroup {
+        padding: 0 8px 8px;
+    }
+    .note {
+        font-size: 12px;
+    }
+    .plugin-icon {
+        width: 48px;
+        height: 48px;
+        margin-right: 16px;
+        border-radius: 4px;
+        background-image: url(${pluginIcon});
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+</style>
+<dialog id="dialog">
+	<form id="form" method="dialog">
+		<h1 id="title">${uiLabel.settingTitle}</h1>
+		<div class="formgroup row">
+			<div class="row">
+				<label for="smallNudge">${uiLabel.labelSmallNudge}</label>
+				<input id="smallNudge" type="number" step="0.1" placeholder="0" value="${defaultVal.normal}" />
+			</div>
+			<div class="row">
+				<label for="bigNudge">${uiLabel.labelBigNudge}</label>
+				<input id="bigNudge" type="number" step="1" placeholder="0" value="${defaultVal.greatly}" />
+			</div>
+		</div>
+		<footer>
+			<button id="cancel">Cancel</button>
+			<button id="ok" type="submit" uxp-variant="cta">OK</button>
+		</footer>
+	</form>
+	<div class="plugin-icon"></div>
+</dialog>
+`;
+	const dialog = dom('#dialog');
+	const form = dom('#form');
+	const smallNudge = dom('#smallNudge');
+	const bigNudge = dom('#bigNudge');
+	const cancel = dom('#cancel');
+	const ok = dom('#ok');
+
+	// Cancel button event
+	const cancelDialog = () => dialog.close('reasonCanceled');
+	cancel.addEventListener('click', cancelDialog);
+	cancel.addEventListener('keypress', cancelDialog);
+
+	// OK button event
+	const confirmedDialog = (e) => {
+		let config = {};
+		config.smallNudge = smallNudge.value;
+		config.bigNudge = bigNudge.value;
+		dialog.close(config);
+		e.preventDefault();
+	};
+	ok.addEventListener('click', confirmedDialog);
+	ok.addEventListener('keypress', confirmedDialog);
+
+	form.onsubmit = confirmedDialog;
+
+	return dialog;
+}
 
 function createAlert(title, msg) {
 	document.body.innerHTML = `
@@ -44,6 +130,66 @@ function createAlert(title, msg) {
 	return dialog;
 }
 
+async function readConfig() {
+	let entry = await openFile();
+
+	if (entry) {
+		return JSON.parse(await entry.read());
+	} else {
+		// Set and return default values if config.json is not found
+		let defaultVal = {"normal": 1, "greatly": 10};
+		const pluginDataFolder = await fs.getDataFolder();
+		const buffer = await pluginDataFolder.createFile(configFile);
+		buffer.write(JSON.stringify(defaultVal));
+
+		return defaultVal;
+	}
+}
+
+async function writeConfig(val) {
+	let entry = await openFile();
+
+	if (entry) {
+		await entry.write(JSON.stringify(val));
+
+		return true;
+	} else {
+		// Create file and write value if config.json is not found
+		const pluginDataFolder = await fs.getDataFolder();
+		const buffer = await pluginDataFolder.createFile(configFile);
+		buffer.write(JSON.stringify(val));
+
+		return true;
+	}
+}
+
+async function openFile() {
+	const pluginDataFolder = await fs.getDataFolder();
+	const entries = await pluginDataFolder.getEntries();
+
+	// Seek a config.json
+	for (let i = 0; i < entries.length; i++) {
+		console.log(entries[i].name);
+		if (configFile === entries[i].name) {
+			return await entries[i];
+		}
+	}
+
+	return false;
+}
+
+function validateNum(val) {
+	if (isNaN(val - 0)) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
 module.exports = {
-	createAlert
+	createSettingDialog,
+	createAlert,
+	readConfig,
+	writeConfig,
+	validateNum
 }
